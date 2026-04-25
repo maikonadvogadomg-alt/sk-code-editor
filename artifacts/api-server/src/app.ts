bf45b032383981bf1c2e0e5aff16418ffb6611b4
maikonadvogadomg-alt <maikon.advogado.mg@gmail.com>
2026-04-25 19:22:47 -0300

    Atualizar o app.ts

--- artifacts/api-server/src/app.ts
+++ artifacts/api-server/src/app.ts
@@ -26,7 +26,7 @@ app.use(
   }),
 );
 app.use(cors());
-app.use(express.json({ limit: "10mb" }));
+app.use(express.json({ limit: "15mb" }));
 app.use(express.urlencoded({ extended: true, limit: "10mb" }));
 
 app.use("/api", router);
